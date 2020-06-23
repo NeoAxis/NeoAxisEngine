4d930a58364cb0a5203d208bb213e5629720ff52
IvanEfimov <IvanEfimov@gmail.com>
2020-06-23 07:35:34 +0200

    ICSharpCode.AvalonEdit

---
+++ Sources/Engine/ICSharpCode.AvalonEdit/AvalonEditDarkThemeUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ICSharpCode.AvalonEdit
+{
+	public static class AvalonEditDarkThemeUtility
+	{
+        public static bool DarkTheme;
+    }
+}
