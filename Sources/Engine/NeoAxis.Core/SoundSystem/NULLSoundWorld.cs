bc15819e3aceeac7fbf41129b6a13b8ea147a766
betauser6 <betauser@mail.ru>
2024-04-06 21:21:46 +0200

    Git fixes

--- Sources/Engine/NeoAxis.Core/SoundSystem/NULLSoundWorld.cs
+++ Sources/Engine/NeoAxis.Core/SoundSystem/NULLSoundWorld.cs
@@ -1,7 +1,6 @@
 // Copyright (C) NeoAxis Group Ltd. 8 Copthall, Roseau Valley, 00152 Commonwealth of Dominica.
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace NeoAxis
 {
